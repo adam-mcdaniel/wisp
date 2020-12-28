b4616a90261432f5a2a15799dc5994a31dfe5fe2
Allen Goodman <allen.goodman@icloud.com>
2020-12-28 13:03:57 -0500

    `const` references
    parameters copied for each invocation but used as `const` references; refactored into `const` references

--- wisp.cpp
+++ wisp.cpp
@@ -95,7 +95,7 @@ std::string read_file_contents(const std::string& filename) {
 #define to_string( x ) static_cast<std::ostringstream&>((std::ostringstream() << std::dec << x )).str()
 
 // Replace a substring with a replacement string in a source string
-void replace_substring(std::string &src, const std::string& substr, std::string replacement) {
+void replace_substring(std::string &src, const std::string& substr, const std::string& replacement) {
     size_t i=0;
     for (i=src.find(substr, i); i!=std::string::npos; i=src.find(substr, i)) {
         src.replace(i, substr.size(), replacement);
@@ -126,11 +126,11 @@ public:
     // have this atom in scope?
     // This is only used to determine which atoms to capture when
     // creating a lambda function.
-    bool has(const std::string& name) const;
+    bool has(std::string name) const;
     // Get the value associated with this name in this scope
-    Value get(const std::string& name) const;
+    Value get(std::string name) const;
     // Set the value associated with this name in this scope
-    void set(const std::string& name, Value value);
+    void set(std::string name, Value value);
 
     void combine(Environment const &other);
 
@@ -187,7 +187,7 @@ public:
     Value(std::vector<Value> list) : type(LIST), list(list) {}
 
     // Construct a quoted value
-    static Value quote(const Value& quoted) {
+    static Value quote(Value quoted) {
         Value result;
         result.type = QUOTE;
 
@@ -332,7 +332,7 @@ public:
     }
 
     // Push an item to the end of this list
-    void push(const Value& val) {
+    void push(Value val) {
         // If this item is not a list, you cannot push to it.
         // Throw an error.
         if (type != LIST)
@@ -448,7 +448,7 @@ public:
         return !(*this < other);
     }
 
-    bool operator<=(const Value& other) const {
+    bool operator<=(Value other) const {
         return (*this == other) || (*this < other);
     }
 
@@ -456,7 +456,7 @@ public:
         return !(*this <= other);
     }
 
-    bool operator<(const Value& other) const {
+    bool operator<(Value other) const {
         // Other type must be a float or an int
         if (other.type != FLOAT && other.type != INT)
             throw Error(*this, Environment(), INVALID_BIN_OP);
@@ -826,7 +826,7 @@ std::ostream &operator<<(std::ostream &os, Environment const &e) {
     return os << "}";
 }
 
-void Environment::set(const std::string& name, Value value) {
+void Environment::set(std::string name, Value value) {
     defs[name] = value;
 }
 
@@ -1680,7 +1680,7 @@ void repl(Environment &env) {
 }
 
 // Does this environment, or its parent environment, have a variable?
-bool Environment::has(const std::string& name) const {
+bool Environment::has(std::string name) const {
     // Find the value in the map
     std::map<std::string, Value>::const_iterator itr = defs.find(name);
     if (itr != defs.end())
@@ -1694,7 +1694,7 @@ bool Environment::has(const std::string& name) const {
 }
 
 // Get the value associated with this name in this scope
-Value Environment::get(const std::string& name) const {
+Value Environment::get(std::string name) const {
     // Meta operations
     if (name == "eval")  return Value("eval",  builtin::eval);
     if (name == "type")  return Value("type",  builtin::get_type_name);
